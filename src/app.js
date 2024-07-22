f3025cb92a18a5f1eef12b0639041d85a453bfbf
Adir <slutzkis@gmail.com>
2024-07-22 12:34:02 +0300

    updates

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Circle from "./Circle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Circle />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
